236e4ac611190e40ad83fa36c667e8655300127f
Yusufege Eren <yusufege.erenn@gmail.com>
2025-07-03 00:31:58 +0300

    Initial Commit

---
+++ AIPdfScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIPdfScanner
+//
+//  Created by Yusufege Eren on 3.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AIPdfScannerTests/AIPdfScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIPdfScannerTests.swift
+//  AIPdfScannerTests
+//
+//  Created by Yusufege Eren on 3.07.2025.
+//
+
+import Testing
+@testable import AIPdfScanner
+
+struct AIPdfScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
